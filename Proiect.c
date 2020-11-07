7882ef7250be6dde7f9648783935de8e66575f18
maximclaudiu <60611723+maximclaudiu@users.noreply.github.com>
2020-11-07 18:56:01 +0200

    finalziare etapa1+continuare

--- Proiect.c
+++ Proiect.c
@@ -11,6 +11,7 @@
 //==============================================================================
 // Include files
 
+#include <advanlys.h>
 #include <ansi_c.h>
 #include <cvirte.h>		
 #include <formatio.h>
@@ -27,7 +28,12 @@
 //==============================================================================
 // Static global variables
 
-static int panelHandle = 0;
+int panelHandle = 0;
+double min, max, sum, median;
+double alpha;
+static int minInd, maxInd, count=0, filtered=0;
+
+
 
 //==============================================================================
 // Static functions
@@ -39,6 +45,7 @@ int waveInfo[2]; //waveInfo[0] = sampleRate
 double sampleRate = 0.0;
 int npoints = 0;
 double *waveData = 0;
+double *waveFiltered = 0;
 
 //==============================================================================
 // Global functions
@@ -89,30 +96,128 @@ int CVICALLBACK panelCB (int panel, int event, void *callbackData,
 int CVICALLBACK OnLoadButtonCB (int panel, int control, int event,
 								void *callbackData, int eventData1, int eventData2)
 {
+	
 	switch (event)
 	{
+		case EVENT_COMMIT:
+			//executa script python pentru conversia unui fisierului .wav in .txt
+			LaunchExecutable("python main.py");
+			
+			//astept sa fie generate cele doua fisiere (modificati timpul daca este necesar
+			Delay(1);
+			
+			//incarc informatiile privind rata de esantionare si numarul de valori
+			FileToArray("wafeInfo.txt", waveInfo, VAL_INTEGER, 2, 1, VAL_GROUPS_TOGETHER, VAL_GROUPS_AS_COLUMNS, VAL_ASCII);
+			sampleRate = waveInfo[SAMPLE_RATE];
+			npoints = waveInfo[NPOINTS];
+			
+			//alocare memorie pentru numarul de puncte
+			waveData = (double *) calloc(npoints, sizeof(double));
+			
+			//incarcare din fisierul .txt in memorie (vector)
+			FileToArray("waveData.txt", waveData, VAL_DOUBLE, npoints, 1, VAL_GROUPS_TOGETHER, VAL_GROUPS_AS_COLUMNS, VAL_ASCII);
+			
+			//afisare pe grapf
+			PlotY(panel, PANEL_RAW_DATA_GRAPH, waveData+count*(int)sampleRate, sampleRate,VAL_DOUBLE, VAL_THIN_LINE, VAL_EMPTY_SQUARE, VAL_SOLID, VAL_CONNECTED_POINTS, VAL_RED);
+			if (filtered)
+				PlotY(panel, PANEL_FILTERED_DATA_GRAPH, waveFiltered+count*(int)sampleRate, sampleRate,VAL_DOUBLE, VAL_THIN_LINE, VAL_EMPTY_SQUARE, VAL_SOLID, VAL_CONNECTED_POINTS, VAL_RED);
 		
-	case EVENT_COMMIT:
-				//executa script python pentru conversia unui fisierului .wav in .txt
-				LaunchExecutable("main.py");
-				
-				//astept sa fie generate cele doua fisiere (modificati timpul daca este necesar
-				Delay(4);
-				
-				//incarc informatiile privind rata de esantionare si numarul de valori
-				FileToArray("wafeInfo.txt", waveInfo, VAL_INTEGER, 2, 1, VAL_GROUPS_TOGETHER, VAL_GROUPS_AS_COLUMNS, VAL_ASCII);
-				sampleRate = waveInfo[SAMPLE_RATE];
-				npoints = waveInfo[NPOINTS];
-				
-				//alocare memorie pentru numarul de puncte
-				waveData = (double *) calloc(npoints, sizeof(double));
-				
-				//incarcare din fisierul .txt in memorie (vector)
-				FileToArray("waveData.txt", waveData, VAL_DOUBLE, npoints, 1, VAL_GROUPS_TOGETHER, VAL_GROUPS_AS_COLUMNS, VAL_ASCII);
-				
-				//afisare pe grapf
-				PlotY(panel, PANEL_Raw_Data_Graph, waveData, npoints, VAL_DOUBLE, VAL_THIN_LINE, VAL_EMPTY_SQUARE, VAL_SOLID, VAL_CONNECTED_POINTS, VAL_RED);
-				
+			MaxMin1D (waveData, npoints, &max, &maxInd, &min, &minInd);
+			SetCtrlVal(panel, PANEL_MAX, max);
+			SetCtrlVal(panel, PANEL_MIN, min);
+			
+			Sum1D (waveData, npoints, &sum);
+			double dispersie = pow(sum, 2)/npoints;
+			double medie=sum/npoints;
+			SetCtrlVal(panel, PANEL_MEDIE, medie);
+			dispersie = dispersie - pow(medie, 2);
+			SetCtrlVal (panel, PANEL_DISPERSIA, dispersie);
+			
+			Median(waveData, npoints, &median);
+			SetCtrlVal(panel, PANEL_MEDIANA, median);
+			
+			break;
+	}
+	return 0;
+}
+
+
+int CVICALLBACK ApplyCB (int panel, int control, int event,
+						 void *callbackData, int eventData1, int eventData2)
+{
+	switch (event)
+	{
+		case EVENT_COMMIT:
+			int choice;
+			filtered=1;
+			DeleteGraphPlot(panel, PANEL_FILTERED_DATA_GRAPH, -1, VAL_IMMEDIATE_DRAW);
+			waveFiltered = (double *) calloc(npoints, sizeof(double));
+			GetCtrlVal(panel, PANEL_Filtru, &choice);
+			if (choice ==16 || choice ==32)
+			{	
+				for (int i=0;i<npoints-choice;i++)
+				{
+					int sum2=0;
+					for (int j=i;j<i+choice;j++)
+						sum2=sum2+waveData[j];
+					sum2=sum2/choice;
+					waveFiltered[i]=sum2;
+				}
+			}
+			else if (choice ==0){
+				GetCtrlVal(panel, PANEL_ALPHA, &alpha);
+	            for(int i = 1; i<npoints; i++)
+	                waveFiltered[i]=(1-alpha)*waveFiltered[i-1]+alpha*waveData[i];
+	      	}
+				PlotY(panel, PANEL_RAW_DATA_GRAPH, waveData+count*(int)sampleRate, sampleRate,VAL_DOUBLE, VAL_THIN_LINE, VAL_EMPTY_SQUARE, VAL_SOLID, VAL_CONNECTED_POINTS, VAL_RED);
+			if (filtered)
+				PlotY(panel, PANEL_FILTERED_DATA_GRAPH, waveFiltered+count*(int)sampleRate, sampleRate,VAL_DOUBLE, VAL_THIN_LINE, VAL_EMPTY_SQUARE, VAL_SOLID, VAL_CONNECTED_POINTS, VAL_RED);
+			break;
+	}
+	return 0;
+}
+
+int CVICALLBACK PrevCB (int panel, int control, int event,
+						void *callbackData, int eventData1, int eventData2)
+{
+	switch (event)
+	{
+		case EVENT_COMMIT:
+				GetCtrlVal (panel, PANEL_Start, &count);
+				if (count>1)
+				{	
+					SetCtrlVal(panel, PANEL_Start, count-1 );
+					SetCtrlVal(panel, PANEL_Stop, count);
+				}
+				count=count-1;
+				DeleteGraphPlot(panel, PANEL_RAW_DATA_GRAPH, -1, VAL_IMMEDIATE_DRAW);
+				DeleteGraphPlot(panel, PANEL_FILTERED_DATA_GRAPH, -1, VAL_IMMEDIATE_DRAW);
+				PlotY(panel, PANEL_RAW_DATA_GRAPH, waveData+count*(int)sampleRate, sampleRate,VAL_DOUBLE, VAL_THIN_LINE, VAL_EMPTY_SQUARE, VAL_SOLID, VAL_CONNECTED_POINTS, VAL_RED);
+				if (filtered)
+					PlotY(panel, PANEL_FILTERED_DATA_GRAPH, waveFiltered+count*(int)sampleRate, sampleRate,VAL_DOUBLE, VAL_THIN_LINE, VAL_EMPTY_SQUARE, VAL_SOLID, VAL_CONNECTED_POINTS, VAL_RED);
+			break;
+	}
+	return 0;
+}
+
+int CVICALLBACK NextCB (int panel, int control, int event,
+						void *callbackData, int eventData1, int eventData2)
+{
+	switch (event)
+	{
+		case EVENT_COMMIT:
+				GetCtrlVal(panel, PANEL_Stop, &count);
+				if (count<5)
+				{
+					SetCtrlVal(panel, PANEL_Start, count );
+					SetCtrlVal(panel, PANEL_Stop, count+1);
+				}
+				DeleteGraphPlot(panel, PANEL_RAW_DATA_GRAPH, -1, VAL_IMMEDIATE_DRAW);
+				DeleteGraphPlot(panel, PANEL_FILTERED_DATA_GRAPH, -1, VAL_IMMEDIATE_DRAW);
+				PlotY(panel, PANEL_RAW_DATA_GRAPH, waveData+count*(int)sampleRate, sampleRate,VAL_DOUBLE, VAL_THIN_LINE, VAL_EMPTY_SQUARE, VAL_SOLID, VAL_CONNECTED_POINTS, VAL_RED);
+				if (filtered)
+					PlotY(panel, PANEL_FILTERED_DATA_GRAPH, waveFiltered+count*(int)sampleRate, sampleRate,VAL_DOUBLE, VAL_THIN_LINE, VAL_EMPTY_SQUARE, VAL_SOLID, VAL_CONNECTED_POINTS, VAL_RED);
+			
 			break;
 	}
 	return 0;
--- Proiect.h
+++ Proiect.h
@@ -15,18 +15,22 @@
 
 #define  PANEL                            1       /* callback function: panelCB */
 #define  PANEL_LoadButton                 2       /* control type: command, callback function: OnLoadButtonCB */
-#define  PANEL_Prev_Button                3       /* control type: command, callback function: (none) */
-#define  PANEL_Next_Button                4       /* control type: command, callback function: (none) */
+#define  PANEL_Prev_Button                3       /* control type: command, callback function: PrevCB */
+#define  PANEL_Next_Button                4       /* control type: command, callback function: NextCB */
 #define  PANEL_Stop                       5       /* control type: numeric, callback function: (none) */
 #define  PANEL_Start                      6       /* control type: numeric, callback function: (none) */
 #define  PANEL_Interval                   7       /* control type: textMsg, callback function: (none) */
-#define  PANEL_Mean                       8       /* control type: numeric, callback function: (none) */
-#define  PANEL_Filtered_Data_Graph        9       /* control type: graph, callback function: (none) */
-#define  PANEL_Raw_Data_Graph             10      /* control type: graph, callback function: (none) */
-#define  PANEL_Filtru                     11      /* control type: ring, callback function: (none) */
-#define  PANEL_Dimensiune                 12      /* control type: numeric, callback function: (none) */
-#define  PANEL_Aplica_Button              13      /* control type: command, callback function: (none) */
-#define  PANEL_TEXTMSG                    14      /* control type: textMsg, callback function: (none) */
+#define  PANEL_DISPERSIA                  8       /* control type: numeric, callback function: (none) */
+#define  PANEL_MEDIANA                    9       /* control type: numeric, callback function: (none) */
+#define  PANEL_MEDIE                      10      /* control type: numeric, callback function: (none) */
+#define  PANEL_MAX                        11      /* control type: numeric, callback function: (none) */
+#define  PANEL_MIN                        12      /* control type: numeric, callback function: (none) */
+#define  PANEL_FILTERED_DATA_GRAPH        13      /* control type: graph, callback function: (none) */
+#define  PANEL_RAW_DATA_GRAPH             14      /* control type: graph, callback function: (none) */
+#define  PANEL_Filtru                     15      /* control type: ring, callback function: (none) */
+#define  PANEL_ALPHA                      16      /* control type: numeric, callback function: (none) */
+#define  PANEL_Aplica_Button              17      /* control type: command, callback function: ApplyCB */
+#define  PANEL_TEXTMSG                    18      /* control type: textMsg, callback function: (none) */
 
 
      /* Control Arrays: */
@@ -41,8 +45,11 @@
 
      /* Callback Prototypes: */
 
+int  CVICALLBACK ApplyCB(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK NextCB(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK OnLoadButtonCB(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK panelCB(int panel, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK PrevCB(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 
 
 #ifdef __cplusplus
